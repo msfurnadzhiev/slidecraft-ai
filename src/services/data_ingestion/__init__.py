cc4bd4d976feb10842800090106cb225ecf36d7c
Miroslav Furnadzhiev <miroslav.furnadzhiev@lights.digital>
2026-03-26 15:18:49 +0200

    remove redundant files

--- src/services/data_ingestion/__init__.py
+++
@@ -1,8 +0,0 @@
-from src.services.data_ingestion.file_loader import FileLoader, PDFLoader
-from src.services.data_ingestion.text_embedder import TextEmbedder
-
-__all__ = [
-    "FileLoader",
-    "PDFLoader",
-    "TextEmbedder",
-]
--- src/storage/__init__.py
+++
@@ -1,7 +0,0 @@
-from src.storage.image_storage import ImageStorage, LocalImageStorage
-
-__all__ = [
-    "ImageStorage",
-    "LocalImageStorage",
-]
-
